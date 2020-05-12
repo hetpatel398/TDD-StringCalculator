13aceb0e7eb11ba03767f50ab30af00b730c7f97
hetpatel398 <hetpatel398@gmail.com>
2020-05-12 14:29:24 +0530

    Added dependencies and created failing test and empty StringCalculator class

---
+++ src/test/java/tdd/kata/StringCalculatorTests.java
@@ -0,0 +1,14 @@
+package tdd.kata;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class StringCalculatorTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
